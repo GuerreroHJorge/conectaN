92853ebe6db72fce0d698dbe1f508ac9276619e1
Jorge Guerrero <89558483+GuerreroHJorge@users.noreply.github.com>
2021-12-07 01:19:36 -0600

    Update conectaN.h
    Con un solo arreglo de movs, creo que está igual xd pero por si las dudas

--- conectaN.h
+++ conectaN.h
@@ -2,8 +2,11 @@
 // Created by Usuario on 04/12/2021.
 //
 
+
 #ifndef CONECTAN_CONECTAN_H
 #define CONECTAN_CONECTAN_H
+
+#include "queue.h"
 
 typedef struct movimiento Movimiento;
 typedef struct juego Juego;
@@ -12,8 +15,6 @@ Juego *nuevoJuego(int N);
 
 void nuevoMovimiento(Juego *juego, int jugador, int renglon, int columna);
 
-void printArreglos(Juego * juego);
-
 int ganoColumna(Juego* juego, int N, Movimiento* mov);
 
 int ganoRenglon(Juego* juego, int N, Movimiento* mov);
@@ -21,6 +22,11 @@ int ganoRenglon(Juego* juego, int N, Movimiento* mov);
 int ganoDiagonalDerecha(Juego* juego, int N, Movimiento* mov);
 
 int ganoDiagonalIzquierda(Juego* juego, int N, Movimiento* mov);
+
+void printArreglos(Juego * juego);
+
+void printArreglos2(Juego * juego);
+
 
 
 
